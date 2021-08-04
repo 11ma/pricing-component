29dc060fd43c026a6a1870222a67d3f47b19f1c5
Michael A <mikeade08l@GMAIL.COM>
2021-08-04 22:47:25 +0100

    update

--- src/App.js
+++ src/App.js
@@ -13,15 +13,24 @@ function App() {
       <p>Sign-up for our 30-day trial. No credit card required.</p>
       <div>
         <div>
-          <p>100k PageViews</p>
-          <p>£{price}/month</p>
+          <div>
+            <p>100k PageViews</p>
+            <p>£{price}/month</p>
+          </div>
           <input
             type="range"
-            min="1"
-            max="100"
+            min="13"
+            step="3"
+            max="19"
             value={price}
             onChange={handleRangeSlider}
           />
+          <div>
+            <label class="switch">
+              <input type="checkbox" />
+              <span class="slider"></span>
+            </label>
+          </div>
         </div>
       </div>
     </div>
